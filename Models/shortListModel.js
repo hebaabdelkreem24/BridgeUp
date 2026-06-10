17e0fbc88512bb6bb4b5fa8d8d6cf51da9f0c0e7
hebaabdelkreem24 <hebaabdelkreem243@gmail.com>
2026-06-10 14:33:37 +0300

    berforenour

--- Models/shortListModel.js
+++ Models/shortListModel.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const roadmapSchema = new mongoose.Schema(
-  {
-    track: {
-      type: String,
-      required: true,
-      enum: ["Frontend", "Backend"],
-      unique: true,
-    },
-  },
-  { timestamps: true }
-);
+// const roadmapSchema = new mongoose.Schema(
+//   {
+//     track: {
+//       type: String,
+//       required: true,
+//       enum: ["Frontend", "Backend"],
+//       unique: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-const Roadmap = mongoose.model("Roadmap", roadmapSchema);
-export default Roadmap;
+// const Roadmap = mongoose.model("Roadmap", roadmapSchema);
+// export default Roadmap;
